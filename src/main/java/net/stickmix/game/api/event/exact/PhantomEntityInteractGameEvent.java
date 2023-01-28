5912ca06f6003550e2a44c1e030a12442e9f6bc0
M4JESTY <majestylgd@gmail.com>
2023-01-28 12:06:33 +0200

    дополнил PhantomEntityInteractGameEvent

--- src/main/java/net/stickmix/game/api/event/exact/PhantomEntityInteractGameEvent.java
+++ src/main/java/net/stickmix/game/api/event/exact/PhantomEntityInteractGameEvent.java
@@ -2,10 +2,14 @@ package net.stickmix.game.api.event.exact;
 
 import lombok.Data;
 import net.stickmix.game.api.event.GameEvent;
+import net.stickmix.game.api.phantom.entity.PhantomEntity;
+import org.bukkit.entity.Player;
 
 @Data
 public class PhantomEntityInteractGameEvent extends GameEvent {
 
+    private final Player whoClicked;
+    private final PhantomEntity phantomEntity;
     private final Type type;
 
     public enum Type {
